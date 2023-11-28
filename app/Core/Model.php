3474e3b80a98ce9a22856059d96d164dc1c52185
Joaquim Martins Scavone <joaquim.scavone@ifto.edu.br>
2023-11-28 15:04:09 -0300

    Update e Delete

--- app/Core/Model.php
+++ app/Core/Model.php
@@ -16,9 +16,12 @@ abstract class Model
 
     private $__data = [];
 
+
     private $where = []; 
 
     protected $pk = 'id';
+
+    private $__storage = false;
 
     public function __construct($id = null){
         if(isset($id)){
@@ -50,17 +53,17 @@ abstract class Model
         $result = $stm->fetch(\PDO::FETCH_ASSOC);
         if($result){
             $this->__data = $result;
+            $this->__storage = true;
         }
     }
 
 
     /**
      * Insererir no banco de dados
-     * @return int
+     * @return self
      */
-    public function insert(array $data = [])
+    private function insert(array $data)
     {
-        $data = array_merge($this->__data, $data);
         $columns = implode(', ',array_keys($data));
         $values = implode(', :',array_keys($data));
         $sql = "INSERT INTO  $this->table ($columns) values (:$values);";
@@ -68,8 +71,9 @@ abstract class Model
         $id = $this->getLastInsertId();
         $pk = $this->pk;
         $this->__data = $data;
+        $this->__storage = true;
         $this->$pk = $id;
-        return $id;
+        return $this;
     }
 
     public function getLastInsertId(){
@@ -77,11 +81,37 @@ abstract class Model
         return $conn->lastInsertId($this->table);
     }
 
-    public function update()
+    private function update(array $data)
     {
+        $sql = "UPDATE $this->table SET";
+        $comma = '';
+        foreach($data as $key => $value){
+            $sql .= "$comma $key = :$key";
+            $comma = ',';
+        }
+        $sql .= " WHERE $this->pk = :w0";
+        $this->query($sql, array_merge($data,['w0'=>$this->{$this->pk}]));
+        $this->__data = $data;
+        return $this;
     }
+    public function save(array $data = []){
+        $data = array_merge($this->__data, $data);
+        if($this->__storage){
+            return $this->update($data);
+        }else{
+            return $this->insert($data);
+        }
+    }
+
     public function delete()
     {
+        if($this->__storage){
+            $sql = "DELETE FROM $this->table WHERE $this->pk = :$this->pk;";
+            $this->query($sql, [$this->pk => $this->{$this->pk}]);
+            $this->__storage = false;
+            return true;
+        }
+        return false;      
     }
 
     private function select(){
@@ -92,11 +122,19 @@ abstract class Model
     }
     public function all()
     {
-        return $this->select()->fetchAll(\PDO::FETCH_CLASS,get_class($this));
+        $result = $this->select()->fetchAll(\PDO::FETCH_CLASS,get_class($this));
+        array_walk($result,function(&$obj){
+            $obj->__storage = true;
+        });
+        return $result;
     }
     public function get()
     {
-        return $this->select()->fetchObject(get_class($this));
+        $result = $this->select()->fetchObject(get_class($this));
+        if($result){
+            $result->__storage = true;
+        }
+        return $result;
     }
 
 
@@ -115,9 +153,9 @@ abstract class Model
         $data = [];
         if(count($this->where)>0){
             $this->where[0][0] = 'WHERE';
-            foreach($this->where as $w){
-                $where .= " $w[0] $w[1] $w[2] :$w[1]";
-                $data[$w[1]] = $w[3];
+            foreach($this->where as $key => $w){
+                $where .= " $w[0] $w[1] $w[2] :w$key";
+                $data["w$key"] = $w[3];
             }
             $this->where = [];
         }
--- public/testes.php
+++ public/testes.php
@@ -5,6 +5,16 @@ use Models\Produto;
 
 require_once('../app/application.php');
 
-$config = new Config(9);
-echo $config->name . " = " . $config->value;
+$config = new Config();
+$config->save(['name'=>'Devsenvolvedor Principal','value'=>'Joaquim Scavone']);
+
+all($config->where('value','like','Joaquim%'));
+
+function all($model){
+    $configs = $model->all();
+    array_walk($configs, function ($config) {
+    echo $config->id." | ".$config->name . " | ".$config->value."<hr>";
+    });
+}
+
 //SELECT * FROM CLIENTE WHERE ID = :ID
